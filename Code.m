13b0af2aac6d2ece43a749482fc2b761b4538a6e
MK-1453 <kocagermehmet2@gmail.com>
2026-03-22 17:33:41 +0000

    finished tast 1

--- Code.m
+++ Code.m
@@ -4,11 +4,10 @@
 
 
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
-clear
-a = arduino('COM5','UNO');
 
-%writeDigitalPin(a,'D12',1) %Max LED
-%writeDigitalPin(a,'DX',0) %LED off
+
+writeDigitalPin(a,'D12',1) %Max LED
+writeDigitalPin(a,'DX',0) %LED off
 
 for n = 1:100  %Blinking LED
     writeDigitalPin(a,'D12',0)
@@ -16,13 +15,85 @@ for n = 1:100  %Blinking LED
     writeDigitalPin(a,'D12',0)
 end
 
+
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+clear
+a = arduino('COM5','UNO');
+%B)
+duration = 600;
 
-% Insert answers here
+no_samples = duration+1;
+%Setting arrays
+time = zeros(1, no_samples);
+voltage = zeros(1,no_samples);
+temp = zeros(1,no_samples);
 
+%Constants
+T_coeff = 0.01; %V/C
+V0 = 0.5; %V
+
+for x = 1:no_samples
+    time(x) = x-1;
+
+    voltage(x) = readVoltage(a, 'A2');
+
+    %Voltage to temp
+    temp(x) = (voltage(x) - V0) / T_coeff;
+
+    Ensuring Measuremnts are taken in 10 mins
+    if x < no_samples
+        pause(1);
+    end
+
+end
+
+%Statistical quantities
+min_temp = min(temp);
+max_temp = max(temp);
+avg_temp = mean(temp);
+
+
+
+%C)
+figure;
+plot(time, temp);
+xlabel('Time (s)');
+ylabel('Temperature (°C)');
+title('Temperature V Time')
+grid on;
+
+%D)
+date = '22/03/36';
+location = 'Nottingham';
+
+output = sprintf('Data logging initiated - %s\nLocation - %s\n\n', date,location);
+
+for min = 0:10
+    index = min * 60 + 1;
+    output = [output sprintf('Minute\t\t%d\nTemperature\t%.2f C\n\n',min,temp(index))];
+end
+
+output = [output sprintf('Max temp\t%.2f C\nMin temp\t%.2f C\nAverage temp\t%.2f C\n\nData logging terminated\n',max_temp,min_temp,avg_temp)];
+
+fprintf('%s',output)
+
+%E)
+
+fileID = fopen('capsule_temperature.txt', 'w');
+
+fprintf(fileID, '%s', output);
+
+fclose(fileID);
+
+%Checking file
+fileID = fopen('capsule_temperature.txt','r');
+fileText = fread(fileID, '*char')';
+fclose(fileID);
+
+disp(fileText)
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 
-% Insert answers here
+
 
 
 %% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [30 MARKS]
